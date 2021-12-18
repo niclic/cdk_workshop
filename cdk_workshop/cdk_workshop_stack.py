2c611a78418770c16e5d299a290109f07e6766d9
Raymond Fallon <git@niclic.com>
2021-12-18 17:11:17 -0500

    Expose endpoints as stack outputs.

--- cdk_workshop/cdk_workshop_stack.py
+++ cdk_workshop/cdk_workshop_stack.py
@@ -1,6 +1,7 @@
 from constructs import Construct
 from aws_cdk import (
     Stack,
+    CfnOutput,
     aws_lambda as _lambda,
     aws_apigateway as apigw,
 )
@@ -9,6 +10,14 @@ from cdk_dynamo_table_view import TableViewer
 from .hitcounter import HitCounter
 
 class CdkWorkshopStack(Stack):
+
+    @property
+    def hc_endpoint(self):
+        return self._hc_endpoint
+
+    @property
+    def hc_viewer_url(self):
+        return self._hc_viewer_url
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
@@ -26,13 +35,23 @@ class CdkWorkshopStack(Stack):
             downstream=my_lambda,
         )
 
-        apigw.LambdaRestApi(
+        gateway = apigw.LambdaRestApi(
             self, 'Endpoint', handler=hello_with_counter._handler,
         )
 
-        TableViewer(
+        tv = TableViewer(
             self, 'ViewHitCounter',
             title='Hello Hits',
             table=hello_with_counter.table,
             sort_by='-hits'
+        )
+
+        self._hc_endpoint = CfnOutput(
+            self, 'GatewayUrl',
+            value=gateway.url
+        )
+
+        self._hc_viewer_url = CfnOutput(
+            self, 'TableViewerUrl',
+            value=tv.endpoint
         )
